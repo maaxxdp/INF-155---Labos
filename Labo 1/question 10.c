b3d12cadb79d769a9a031ead6465c1b22531a61f
Maxime Douangpanya <maxime.douangpanya@gmail.com>
2026-04-04 00:11:14 -0400

    Fini le TP1
    Terminer les 10 questions du TP1

--- Labo 1/question 10.c
+++ Labo 1/question 10.c
@@ -1,3 +1,42 @@
-//
-// Created by Max on 2026-04-03.
-//
+#include <stdio.h>
+
+int main(void) {
+    // Variables
+
+    double ciment_in;
+    double sable_in;
+    double gravier_in;
+
+    double ratio_ciment;
+    double ratio_sable;
+    double ratio_gravier;
+
+
+
+    // Programme
+    printf("Ciment: \n");
+    scanf("%lf", &ciment_in);
+
+    printf("Sable: \n");
+    scanf("%lf", &sable_in);
+
+    printf("Gravier: \n");
+    scanf("%lf", &gravier_in);
+
+    ratio_ciment = ciment_in/350;
+    ratio_sable = sable_in/680;
+    ratio_gravier = gravier_in/1175;
+
+    double min = ratio_ciment;
+    if (ratio_sable < min) {
+        min = ratio_sable;
+
+    }
+    if (ratio_gravier < min) {
+        min = ratio_gravier;
+
+    }
+    printf("Le volume maximum est de %.2f", min);
+
+
+}
